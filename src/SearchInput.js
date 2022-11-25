3b7b6537ae17529b4e2943fc655607e7667a0023
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-11-25 23:56:42 +0530

    cmnt

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -2,7 +2,6 @@ import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 
 import "./SearchInput.css";
-
 export default class SearchInput extends PureComponent {
 
   static propTypes = {
