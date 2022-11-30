6caf6ccd31c6180926a4c7ec504a2ad2a24a6524
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-11-30 23:37:42 +0530

    cmnt

--- src/EmojiResults.js
+++ src/EmojiResults.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import Clipboard from "clipboard";
-
 import EmojiResultRow from "./EmojiResultRow";
 import "./EmojiResults.css"
 export default class EmojiResults extends PureComponent {
